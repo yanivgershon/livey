c550d6938eed60da9e45b1569fb79db7edf71188
yaniv gershon <yanivger@walla.co.il>
2020-04-29 12:41:09 +0300

    add 2 fields to item

--- StreamHubCore2/StreamHubCoreDal/Model/Item.cs
+++ StreamHubCore2/StreamHubCoreDal/Model/Item.cs
@@ -30,6 +30,7 @@ namespace StreamHubCoreDal.Models
         [Column(TypeName = "bigint")]
         public int ItemStartDate { get; set; }
         public DateTime ItemStartDateObj { get; set; }
+        public DateTime ItemEndDateObj { get; set; }
         public int ItemDuration { get; set; }
 
         public string ItemOwner { get; set; }
@@ -37,6 +38,7 @@ namespace StreamHubCoreDal.Models
         public int PlatformID { get; set; }
 
         public string ItemImgURL { get; set; }
+        public string ScarpingSource { get; set; }
 
         public virtual ICollection<Category> Categories { get; set; }
         [NotMapped]
