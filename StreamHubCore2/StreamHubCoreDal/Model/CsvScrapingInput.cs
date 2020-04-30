6a8e8c37f326f23bb655884e2ac25fd80dc82b9d
yaniv gershon <yanivger@walla.co.il>
2020-04-30 22:54:06 +0300

    insert CSV to Db

---
+++ StreamHubCore2/StreamHubCoreDal/Model/CsvScrapingInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StreamHubCoreDal.Model
+{
+    public class CsvScrapingInput
+    {
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Title { get; set; }
+        public string Caterogies { get; set; }
+        public string Url { get; set; }
+    }
+}
--- StreamHubCore2/StreamHubCoreJobs/Program.cs
+++ StreamHubCore2/StreamHubCoreJobs/Program.cs
@@ -1,17 +1,26 @@
 ﻿using CsvHelper;
+using Microsoft.EntityFrameworkCore;
+using StreamHubCoreDal.Model;
 using StreamHubCoreDal.Models;
 using System;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 
 namespace StreamHubCoreJobs
 {
     class Program
     {
-        // private static LiveyTvContext liveyTvContext = new LiveyTvContext();
+        private static LiveyTvContext liveyTvContext;
         static void Main(string[] args)
         {
-            iterateAllFiles("D:\\programming3\\skyCommerceV2\\SkyCommerceMigration\\csv");
+            
+            var optionsBuilder = new DbContextOptionsBuilder<LiveyTvContext>();
+            StreamHubCoreDal.AppConfig.AppConfiguration appConfiguration = new StreamHubCoreDal.AppConfig.AppConfiguration();
+
+            optionsBuilder.UseSqlServer(appConfiguration.ConnectionString);
+            liveyTvContext = new LiveyTvContext(optionsBuilder.Options);
+            iterateAllFiles("D:\\programming3\\livey\\Data-Scrapping\\Data");
         }
         private static void iterateAllFiles(string targetDirectory)
         {
@@ -31,18 +40,87 @@ namespace StreamHubCoreJobs
 
         private static void insertCsvContentToDb(string filePath)
         {
+            int count = 0;
+            var line = "";
+            string fileName = filePath.Split("\\").Last().ToString();
+            liveyTvContext.RemoveRange(liveyTvContext.Items.Where(x => x.ScarpingSource == fileName));
             using (var reader = new StreamReader(filePath))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
-                csv.Configuration.HasHeaderRecord = false;
-                //  var records = csv.GetRecords<CsvScrapingInput>();
-                //  records.ToList().ForEach(line =>
-                // {
+                while (( line = reader.ReadLine()) != null)
+                {
+                    if (count > 0)
+                    {
+                        var spl = line.Split(".,");
+                        string Date = spl[0];
+                        string Time = spl[1];
+                        string Title = spl[2];
+                        string Caterogies = spl[3];
+                        string Url = spl[4];
+                        var dateTime = convertDateAndTimeToDateTime(Date, Time);
 
 
-                // }
+                        Item item = new Item
+                        {
+                            createDate = DateTime.Now,
+                            Categories = null,
+                            ItemDescription = "",
+                            ItemDuration = 0,
+                            ItemEndDateObj = dateTime,
+                            ItemImgURL = "",
+                            ItemOwner = "",
+                            ItemStartDate = 0,
+                            ItemStartDateObj = dateTime,
+                            ItemTags = Caterogies,
+                            ItemTitle = Title,
+                            ItemURL = Url,
+                            PlatformID = 1,
+                            ScarpingSource = fileName
+                        };
+                        liveyTvContext.Add(item);
+                    }
+                    count++;
+                }
+                liveyTvContext.SaveChanges();
             }
 
+/*
+            using (var csv = new CsvReader(reader,  CultureInfo.InvariantCulture))
+            {
+                csv.Configuration.Delimiter = ".,";
+                csv.Configuration.HasHeaderRecord = true;
+                var records = csv.GetRecords<CsvScrapingInput>();
+                liveyTvContext.RemoveRange(liveyTvContext.Items.Where(x => x.ScarpingSource == fileName));
+                records.ToList().ForEach(line =>
+               {
+                   Item item = new Item
+                   {
+                       createDate = DateTime.Now,
+                       Categories = null,
+                       ItemDescription = "",
+                       ItemDuration = 0,
+                       ItemEndDateObj = new DateTime(),
+                       ItemImgURL = "",
+                       ItemOwner = "",
+                       ItemStartDate = 0,
+                       ItemStartDateObj = new DateTime(),
+                       ItemTags = line.Caterogies,
+                       ItemTitle = line.Title,
+                       ItemURL = line.Url,
+                       PlatformID = 1,
+                       ScarpingSource = fileName
+                   };
+                   liveyTvContext.Add(item);
+
+               });
+            }
+            */
+        }
+        private static DateTime convertDateAndTimeToDateTime(string Date,string Time)
+        {
+            var splDate = Date.Split(".").Select(x => Int32.Parse(x)).ToArray();
+            var splTime = Time.Split(":").Select(x => Int32.Parse(x)).ToArray();
+
+            return new DateTime(splDate[2], splDate[1], splDate[0], splTime[0], splTime[1], 0);
         }
     }
 }
